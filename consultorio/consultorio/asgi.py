474ab3bff8bc71aee82caf0c0d8385d8c2acf25b
Roberto Franco <roberto.yuri.franco@gmail.com>
2024-01-27 21:31:27 -0300

    Configuração do django; criação do app; configuração dos arquivos statics; configuração do Templates; configuração do banco de dados configuração do superusario criou uma rota url criou base.html criou menu e footer criou home

--- consultorio/consultorio/asgi.py
+++ consultorio/consultorio/asgi.py
@@ -10,7 +10,8 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'consultorio.settings')
 
-application = get_asgi_application()
+application = Cling(MediaCling(get_asgi_application()))
